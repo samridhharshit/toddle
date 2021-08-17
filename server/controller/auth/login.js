68d18e0adf1612b363d79b1bf4e61f00816b414c
samridh <samridhharsh@gmail.com>
2021-08-17 21:25:11 +0530

    route working check added.

---
+++ server/controller/auth/login.js
@@ -0,0 +1,5 @@
+const login = async () => {
+    return "login data"
+}
+
+module.exports = login
--- server/routes/api/auth/login.js
+++ server/routes/api/auth/login.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const login = require("../../../controller/auth/login");
 const router = express.Router()
 
 // logging in a user/restaurant - put request
-router.put('/login', async (req, res) => { res.send(await login(req.body)) })
+router.get('/', async (req, res) => { res.send(await login(req.body)) })
 
 module.exports = router
