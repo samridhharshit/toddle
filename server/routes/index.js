4e5af106d232ed96264b27072de44d3d16929b3a
samridh <samridhharsh@gmail.com>
2021-08-20 10:04:04 +0530

    root api msg added

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.use("/api", require('./api'))
+router.get("/", (req, res) => {
+    res.send("Welcome to toddle assignment mainframe api")
+})
 
 module.exports = router
